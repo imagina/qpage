fbfa307e12abed77fd076a54362787af62a1ea85
msolanogithub <msolano@imaginacolombia.com>
2024-02-12 09:37:38 -0500

    Updated layouts imports

--- _config/adminPages.js
+++ _config/adminPages.js
@@ -7,7 +7,7 @@ export default {
     name: 'qpage.admin.pages',
     crud : import('@imagina/qpage/_crud/pages'),
     page: () => import('@imagina/qcrud/_pages/admin/crudPage'),
-    layout: () => import('@imagina/qsite/_layouts/master.vue'),
+    layout: () => import('layouts/master.vue'),
     title: 'page.cms.sidebar.adminPages',
     icon: 'fa-light fa-browsers',
     subHeader: {
