a6241171aa3c0ec55a1aaaebf12c03e5462d55da
hrodriguezimagina <hrodriguez@imaginacolombia.com>
2024-06-26 16:47:04 -0500

    + added translations

--- _i18n/es/index.js
+++ _i18n/es/index.js
@@ -1,7 +1,15 @@
-import layout from 'modules/qpage/_i18n/es/layout'
-import sidebar from 'modules/qpage/_i18n/es/sidebar'
-
 export default {
-  layout,
-  sidebar
+  "page": {
+    "cms": {
+      "sidebar": {
+        "adminPages": "Paginas"
+      },
+      "newPage": "Nueva pagina",
+      "updatePage": "Actualizar pagina",
+      "page": "Pagina",
+      "documentation": {
+        "pages": "Puedes administrar las paginas estáticas de tu sitio WEB de forma que modifiques el contenido que en está se muestra, como títulos, párrafos, colores de texto, agregar imágenes, tec..."
+      }
+    }
+  }
 }
